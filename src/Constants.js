7b627b4bf0cefd5336bab981e3fbae2c08ffa10c
Kyle Adams <kyle.adams@massnerder.io>
2022-06-09 12:41:03 -0400

    add env vars

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 export const THEME_COLOR = "#000000";
 export const EXPRESS_SERVER_HOST = `https://${process.env.REACT_APP_HOST}.herokuapp.com`;
-export const ENTERPRISE_ID = '23477669';
-export const METADATA_TEMPLATE_KEY = 'member';
+export const ENTERPRISE_ID = process.env.ENTERPRISE_ID;
+export const METADATA_TEMPLATE_KEY = process.env.METADATA_TEMPLATE_KEY;
 
 // export const EXPRESS_SERVER_HOST = "http://localhost:8080";
